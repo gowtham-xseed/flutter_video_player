06cb694f72d34547c7fa5bbd1b19e89f02f0e647
Gowtham <gowtham.p@in.reportbee.com>
2020-06-08 15:27:45 +0530

    Base video player integration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,68 +1,115 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_video_player/flutter_video_player.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(VideoApp());
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'http://mirrors.standaloneinstaller.com/video-sample/page18-movie-4.m4v')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter = Calculator().addOne(_counter);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          //
-          //
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: VideoPlayer(
+            _controller,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      )
     );
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_video_player/flutter_video_player.dart';
+// import 'package:video_player/video_player.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Video Player',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   VideoPlayerController _controller = VideoPlayerController.network(
+//           'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4');
+
+//   @override
+//   void initState() {
+//     _controller.play();
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Column(
+//           children: <Widget>[
+//             Text('hello'),
+//             Container(
+//               child: _controller.value.initialized
+//                   ? AspectRatio(
+//                       aspectRatio: _controller.value.aspectRatio,
+//                       child: VideoPlayer(_controller),
+//                     )
+//                   : Container(),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/components/video_player.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class FlutterVideoPlayer extends StatelessWidget {
+  FlutterVideoPlayer({this.videoPlayerController});
+
+  final VideoPlayerController videoPlayerController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: _buildPlayerWithControls(videoPlayerController, context),
+      ),
+    );
+  }
+
+  Container _buildPlayerWithControls(
+      VideoPlayerController videoPlayerController, BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(),
+          Center(
+            child: AspectRatio(
+              aspectRatio: _calculateAspectRatio(context),
+              child: VideoPlayer(videoPlayerController),
+            ),
+          ),
+          Container(),
+          _buildControls(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControls(
+    BuildContext context,
+  ) {
+    return Container(child: Text('Actions'));
+  }
+
+  double _calculateAspectRatio(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    return width > height ? width / height : height / width;
+  }
+}
